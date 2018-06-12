d8dd122c50715846323543059856be51749326d4
knosence <nadario.seays@gmail.com>
2018-06-12 11:55:31 -0400

    first commit

---
+++ esperantosite-server/src/main/java/com/knosence/esperantosite/esperantosite/EsperantositeApplication.java
@@ -0,0 +1,12 @@
+package com.knosence.esperantosite.esperantosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperantositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsperantositeApplication.class, args);
+	}
+}
---
+++ esperantosite-server/src/main/java/com/knosence/esperantosite/esperantosite/config/CommonConfig.java
@@ -0,0 +1,22 @@
+package com.knosence.esperantosite.esperantosite.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class CommonConfig {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+
+    }
+}
